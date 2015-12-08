f835fa38fd3ba968188260ff9af3c79cfce47d80
fabwach <fabien@v-cult.com>
2015-12-08 12:26:39 +0100

    my first website page

---
+++ index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=1.0">
+		<link rel="stylesheet" href="main.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="main.js"></script>
+	  	<title>Wacheux Fabien - Web Developer & Manager</title>
+	</head>
+	<body>
+		<div id="wrapper">
+		  	<div id="content">
+		    	<div id="card">
+		      		<div id="front">
+		        		<div id="arrow">
+		        			<i class="fa fa-arrow-left"></i>
+		        		</div>
+		        		<div id="top-pic"></div>
+		        		<div id="avatar"></div>
+		        		<div id="info-box">
+		          			<div class="info">
+		            			<h1>Fabien Wacheux</h1>
+		            			<h2>Web Developer & Manager</h2>
+		          			</div>
+		        		</div>
+		        		<div id="social-bar">
+						  	<a href="https://www.facebook.com/fabien.wacheux" target="_blank">
+						    	<i class="fa fa-facebook"></i>
+						  	</a>
+						  	<a href="https://twitter.com/fabienwacheux" target="_blank">
+						  		<i class="fa fa-twitter"></i>
+						  	</a>
+						  	<a href="https://github.com/fabwach" target="_blank">
+						  		<i class="fa fa-github"></i>
+						  	</a>
+						  	<a href="https://www.linkedin.com/in/fabienwacheux" target="_blank">
+						  		<i class="fa fa-linkedin"></i>
+						  	</a>
+						  	<a href="javascript:void" class="more-info">
+						   		<i class="fa fa-user"></i>
+						  	</a>
+						</div>
+		      		</div>
+		      		<div id="back">
+		        		<div class="back-info">
+		          			<p>My name is Fabien Wacheux, and I'm producer for V-Cult, based at Euratechnologies in Lille, France. V-Cult is focus on Virtual Reality and develops a disruptive 3D web technology, fully browser-based, requiring no download and available on all OS and devices. V-Cult edits especially Beloola, the first fully web-based immersive platform to connect like-minded people anywhere and anytime they want. As for me, I'm in the same time web developer & manager. I'm working on new web technologies</p>
+		        		</div>
+		        		<div id="social-bar">
+						  	<a href="https://www.facebook.com/fabien.wacheux" target="_blank">
+						    	<i class="fa fa-facebook"></i>
+						  	</a>
+						  	<a href="https://twitter.com/fabienwacheux" target="_blank">
+						  		<i class="fa fa-twitter"></i>
+						  	</a>
+						  	<a href="https://github.com/fabwach" target="_blank">
+						  		<i class="fa fa-github"></i>
+						  	</a>
+						  	<a href="https://www.linkedin.com/in/fabienwacheux" target="_blank">
+						  		<i class="fa fa-linkedin"></i>
+						  	</a>
+						  	<a href="javascript:void" class="more-info">
+								<i class="fa fa-undo"></i>
+							</a>
+						</div>
+		      		</div>
+		    	</div>
+		    	<div id="background">
+		      		<div id="background-image"></div>
+		    	</div>
+		  	</div>
+		</div>
+	</body>
+</html>
